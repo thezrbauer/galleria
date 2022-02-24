97657f3aafd6b9d10a761528e83992baf81d481e
Zack Bauer <zack.bauer30@gmail.com>
2022-02-24 16:07:11 -0500

    galleria

--- public/script.js
+++ public/script.js
@@ -1,12 +1,18 @@
 var elem = document.querySelector('.grid');
+
 var msnry = new Masonry( elem, {
   // options
   itemSelector: '.grid-item',
-  columnWidth: 200
+  columnWidth: '.grid-sizer'
 });
 
 // element argument can be a selector string
 //   for an individual element
 var msnry = new Masonry( '.grid', {
   // options
+});
+
+imagesLoaded( elem ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
 });
